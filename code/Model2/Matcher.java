44aeff783441d7b927fcf0d3d9370e0061078adc
Mikael Hagafors <hagafors@gmail.com>
2016-12-06 12:05:37 +0100

    uhhhhhh

--- code/Model2/Matcher.java
+++ code/Model2/Matcher.java
@@ -1,53 +1,27 @@
 
 import java.util.*;
 
-/**
- * 
- */
-public class Matcher {1} {
+public class Matcher {
 
-    /**
-     * Default constructor
-     */
-    public Matcher {1}() {
+    public Matcher() {
     }
 
 
-
-
-    /**
-     * @param Job 
-     * @return
-     */
-    public Match MatchJob(void Job) {
+    public Match MatchJob(Job job) {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @param Job 
-     * @param Responder 
-     * @return
-     */
-    private boolean sendQuery(void Job, void Responder) {
+    private boolean sendQuery(Job job, Responder responder) {
         // TODO implement here
         return false;
     }
 
-    /**
-     * @param Job 
-     * @param Requester 
-     * @param Responder 
-     * @return
-     */
-    private Match createMatch(void Job, void Requester, void Responder) {
+    private Match createMatch(Job job, Requester requester, Responder responder) {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @return
-     */
     private void invoiceTrigger() {
         // TODO implement here
         return null;
