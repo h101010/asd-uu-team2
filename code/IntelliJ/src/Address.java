74d6a4d95083e79241bdeae796ec8b6ff738433a
Sara Back <saramyback@gmail.com>
2016-12-07 16:13:39 +0100

    lots of changes after a lot of time

--- code/IntelliJ/src/Address.java
+++ code/IntelliJ/src/Address.java
@@ -1,3 +1,46 @@
 
 public class Address {
+ private String street;
+ private String city;
+ private String postcode;
+ private String country;
+
+    public Address(String street, String city, String postcode, String country) {
+        this.street = street;
+        this.city = city;
+        this.postcode = postcode;
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
